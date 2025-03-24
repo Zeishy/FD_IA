e97e9ace8efa789e22989897a89fabcd51cfe53e
Cédric <cedric.hoarau-de-boisvilliers@epitech.eu>
2025-03-24 11:24:09 +0400

    [ADD]: push

--- main.py
+++ main.py
@@ -4,23 +4,20 @@ from getcat import *
 
 def create_model():
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(3,)),  # Couche d'entrée avec 3 caractéristiques
-        tf.keras.layers.Dense(32, activation='relu'),  # Couche cachée
-        tf.keras.layers.Dense(1)  # Couche de sortie pour prédire le prix
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(3,)),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1)
     ])
     model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
 def housing_AI(houses: list[House]) -> None:
-    # Préparer les données pour l'entraînement
     X = [[house.size, house.num_rooms, house.location_quality] for house in houses]
     y = [house.price for house in houses]
 
-    # Créer et entraîner le modèle
     model = create_model()
     model.fit(X, y, epochs=500, verbose=1)
 
-    # Prédire les prix avec le modèle entraîné
     predictions = model.predict(X)
     for house, predicted_price in zip(houses, predictions):
         print(f"Predicted price for house with size {house.size}, {house.num_rooms} rooms, and location quality {house.location_quality}: {predicted_price[0]:.2f}")
